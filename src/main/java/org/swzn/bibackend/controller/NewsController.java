40c760673b0bb205b88ee45743dd04d69f7b6014
L-R-C-C-M <liuruimeng291@163.com>
2023-06-11 00:20:07 +0800

    最终版

--- src/main/java/org/swzn/bibackend/controller/NewsController.java
+++ src/main/java/org/swzn/bibackend/controller/NewsController.java
@@ -50,7 +50,188 @@ public class NewsController {
     public String test(){
         return "hello!";
     }
+    @GetMapping("/GetNewsById")
+    public Result GetNewsById(String id){
+        Result result=new Result();
+        QueryWrapper<Click> wrapper=new QueryWrapper<>();
+        wrapper.eq("UserId",id);
+        List<Click> newsList=clickMapper.selectList(wrapper);
+        List<String> temp=new ArrayList<>();
+        for(Click n:newsList){
 
+            temp.add(n.getClicknews());
+        }
+        result.data.put("id",temp);
+        return result;
+    }
+
+    //日期转Int
+    public static int convertDateStringToInt(String dateString) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            Date date = sdf.parse(dateString);
+            return Integer.parseInt(new SimpleDateFormat("yyyyMMdd").format(date));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+    public static int convertStringToStartstamp(String dateString) {
+        try {
+            String dateTimeString = dateString + " 00:00:00";
+
+            // 定义日期格式
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+            // 解析字符串为日期对象
+            Date date = sdf.parse(dateTimeString);
+
+            // 获取时间戳（以秒为单位）
+            long timestamp = date.getTime() / 1000;
+
+            // 将时间戳转换为int类型
+            int timestampInt = (int) timestamp;
+
+            return timestampInt;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return 0; // 如果转换失败，返回0或其他默认值
+    }
+    public static int convertStringToEndstamp(String dateString) {
+        try {
+            String dateTimeString = dateString + " 23:59:59";
+
+            // 定义日期格式
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+            // 解析字符串为日期对象
+            Date date = sdf.parse(dateTimeString);
+
+            // 获取时间戳（以秒为单位）
+            long timestamp = date.getTime() / 1000;
+
+            // 将时间戳转换为int类型
+            int timestampInt = (int) timestamp;
+
+            return timestampInt;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return 0; // 如果转换失败，返回0或其他默认值
+    }
+
+
+    //推荐新闻
+    @GetMapping("/Recommend")
+    public Result getRecommend(String userId, String date) {
+        Result result = new Result();
+
+        int intstartdate = convertStringToStartstamp(date);
+        int intenddate = convertStringToEndstamp(date);
+
+        // 查询用户当天点击过的所有新闻
+        QueryWrapper<ClicksInt> clickWrapper = new QueryWrapper<>();
+        clickWrapper.eq("UserId", userId)
+                .between("ClickTime", intstartdate,  intenddate);
+        List<ClicksInt> clickList = clicksIntMapper.selectList(clickWrapper);
+
+        // 统计每个Category的点击量
+        Map<String, Integer> categoryCounts = new HashMap<>();
+        for (ClicksInt click : clickList) {
+            String category = click.getCategory();
+            categoryCounts.put(category, categoryCounts.getOrDefault(category, 0) + 1);
+        }
+
+        // 获取点击量最高的Category
+        String maxCategory = null;
+        int maxCount = 0;
+        for (Map.Entry<String, Integer> entry : categoryCounts.entrySet()) {
+            if (entry.getValue() > maxCount) {
+                maxCategory = entry.getKey();
+                maxCount = entry.getValue();
+            }
+        }
+
+        // 检索点击量最高的Category的新闻的标题和内容（随机3条）
+        QueryWrapper<News> newsWrapper = new QueryWrapper<>();
+        newsWrapper.eq("Category", maxCategory)
+                .last("ORDER BY RAND()")
+                .last("LIMIT 5");// 随机排序并限制返回结果为3条记录
+        List<News> newsList = newsMapper.selectList(newsWrapper);
+
+        // 封装结果
+        List<Map<String, Object>> newsData = new ArrayList<>();
+        for (News news : newsList) {
+            Map<String, Object> newsInfo = new HashMap<>();
+            newsInfo.put("title", news.getHeadline());
+            newsInfo.put("content", news.getNewsBody());
+            newsData.add(newsInfo);
+        }
+        result.data.put("news", newsData);
+
+        return result;
+    }
+
+    //组合查询List<String> userIds
+    @GetMapping("/CombineSearch")
+    public Result getCombineSearch(String userId, String date, String category, Integer headlineLength, Integer newsBodyLength) {
+        Result result = new Result();
+
+        int intstartdate = convertStringToStartstamp(date);
+        int intenddate = convertStringToEndstamp(date);
+
+        // 查询当天用户点击过的新闻ID
+        QueryWrapper<ClicksInt> clickWrapper = new QueryWrapper<>();
+        clickWrapper.eq("UserId", userId)
+                .between("ClickTime", intstartdate,  intenddate);
+        List<ClicksInt> clickList = clicksIntMapper.selectList(clickWrapper);
+
+        List<String> newsIds = new ArrayList<>();
+        for (ClicksInt click : clickList) {
+            newsIds.add(click.getClicknews());
+        }
+
+        // 构建News表的查询条件
+        QueryWrapper<News> newsWrapper = new QueryWrapper<>();
+        newsWrapper.in("News_ID", newsIds);
+
+        // 添加新闻主题的查询条件
+        if (category != null && !category.isEmpty()) {
+            newsWrapper.eq("Category", category);
+        }
+
+        // 添加新闻标题长度的查询条件
+        if (headlineLength != null) {
+            newsWrapper.apply("LENGTH(Headline) < " + headlineLength);
+        }
+
+        // 添加新闻内容长度的查询条件
+        if (newsBodyLength != null) {
+            newsWrapper.apply("LENGTH(News_Body) < " + newsBodyLength);
+        }
+
+        // 随机返回最多3条符合条件的新闻
+        newsWrapper .last("ORDER BY RAND()")
+                .last("LIMIT 5");// 随机排序并限制返回结果为3条记录
+
+        // 检索新闻的标题和内容
+        List<News> newsList = newsMapper.selectList(newsWrapper);
+
+        // 封装结果
+        List<Map<String, Object>> newsData = new ArrayList<>();
+        for (News news : newsList) {
+            Map<String, Object> newsInfo = new HashMap<>();
+            newsInfo.put("title", news.getHeadline());
+            newsInfo.put("content", news.getNewsBody());
+            newsData.add(newsInfo);
+        }
+
+        result.data.put("news", newsData);
+        return result;
+    }
     //新闻点击量变化
     @GetMapping("/NewsClickCount")
     public Result getNewsClickCount(String newsId, String startDate, String endDate) {
